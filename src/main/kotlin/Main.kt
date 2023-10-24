ccc2d79411271222799e5764d7a894aa80108e81
gugatevzadze@gmail.com <gugatevzadze@gmail.com>
2023-10-24 13:34:38 +0400

    added input validation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,39 @@
-fun main(args: Array<String>) {
+fun main() {
 
     var answer:String
-    do { //do-while loop გამოვიყენე რადგან კოდი ერთხელ მაინც უნდა შესრულებილიყო სანამ ლუპი დამთავრდებოდა
-        println("შეიყვანეთ მნიშვნელობა X ცვლადისთვის")
-        val inputX = readln()
-        val X = inputX.toInt() //toInt ფუნქციის გამოყენება საჭიროა რადგან readline() ინპუტად სტრინგს იღებს
 
-        println("შეიყვანეთ მნიშვნელობა Y ცვლადისთვის")
-        val inputY = readln()
-        val Y = inputY.toInt()
+    //do-while loop გამოვიყენე რადგან კოდი ერთხელ მაინც უნდა შესრულებილიყო სანამ ლუპი დამთავრდებოდა
+    do {
+        var x:Int
+        var y:Int
 
-        val Z: Int = X + Y
-        println("X და Y ჯამი არის: ${Z}")
+        //X ინპუტის ვალიდაცია
+        while (true){
+            println("შეიყვანეთ მნიშვნელობა X ცვლადისთვის:")
+            val inputX = readln()
+            try {
+                x = inputX.toInt() //inputX-ს ინტეჯერად გარდაქმნა, რადგან readln() სტრინგს აბრუნებს
+                break
+            }catch (e: NumberFormatException){ //ექსეფშენ ჰენდლინგი. თუ ვერ მოხერხდება სტრინგის ინტეჯერად გარდაქმნა ამოაგდებს მესიჯს
+                println("შეყვანილი მნიშვნელობის დამუშავება ვერ ხერხდება, ხელახლა სცადეთ")
+            }
+        }
+        //Y ინპუტის ვალიდაცია
+        while (true){
+            println("შეიყვანეთ მნიშვნელობა Y ცვლადისთვის:")
+            val inputY = readln()
+            try {
+                y = inputY.toInt()
+                break
+            }catch (e: NumberFormatException){
+                println("შეყვანილი მნიშვნელობის დამუშავება ვერ ხერხდება, ხელახლა სცადეთ")
+            }
+        }
+        val z: Int = x + y
+        println("X და Y ჯამი არის: $z")
 
-        println("გსურთ პროგრამის ხელახლა დაწყება <Y/N>")
-        answer = readln()
+        println("გსურთ პროგრამის ხელახლა დაწყება <Y/N>:")
+        answer = readln().uppercase()//.uppercase() დავამატე რადგან lowercase y-ზეც ამთავრებდა პროგრამას
 
-    } while (answer == "Y")//ამეორებს ლუპს სანამ პასუხი "Y" იქნება, "N"-ს შემთხვევაში ლუპიდან გამოვა და პროგრამა დასრულდება
+    } while (answer == "Y")//ამეორებს ლუპს სანამ პასუხი "Y" იქნება
 }
